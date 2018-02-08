2a844a6dfcb826fbc61d3e14dadc2636a9b4b142
laser010 <mosa.salman.g@gmail.com>
2018-02-09 00:03:48 +0200

    v1.2.0

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # sniper-h
 
 sniper-h in an cracking tool ,Types hash cracking :
-md5
-md4
-sha1
-sha224
-sha256
-sha512
-sha384
-sha3_224
-sha3_256
-sha3_384
-sha3_512
-blake2b
-blake2s
+md5,
+md4,
+sha1,
+sha224,
+sha256,
+sha512,
+sha384,
+sha3_224,
+sha3_256,
+sha3_384,
+sha3_512,
+blake2b,
+blake2s.
 .
 # Requirements:
 
@@ -25,17 +25,18 @@ https://www.python.org/downloads/
 ```
 git clone https://github.com/laser010/sniper-h
 cd sniper-h
-python3 sniper-h.py -h
+python sniper-h.py -h
 ```
 
-# Example
+# Examples
 
 ```
-python sniper-h.py -H 644fef65c574baeda79b3988c2353290 -W wordlist.txt -T md5 
+python sniper-h.py -H 644fef65c574baeda79b3988c2353290 -w wordlist.txt -t md5
+python sniper-h.py -H 644fef65c574baeda79b3988c2353290 -m 7 -cns laser01 -t md5
 ```
 
 # Abut
 
 Instagram : https://www.instagram.com/laser01/
 
-# Happy cracking!
+# Good luck!
--- sniper-h.py
+++ sniper-h.py
@@ -3,6 +3,9 @@
 import hashlib
 import datetime
 import argparse
+import itertools
+from array import array
+import string
 
 print("""
 
@@ -15,26 +18,24 @@ print("""
              | |                           
              |_|                           
 
-	 Coded by @laser010 Version 1.1.1
+	 Coded by @laser010 Version 1.2.0
 
 """)
 
 def main():
 	try:
-		parser = argparse.ArgumentParser(prog="sniper-h.py", add_help=True, usage=(
-		"python sniper-h.py [-H HASH] [-W PATH WORDLIST] [-T TYPYE OF HASH]"))
+		parser = argparse.ArgumentParser(prog="sniper-h.py", add_help=True , usage=(
+		"sniper-h.py [-h] -H HASH [-w WORDLIST] [-m MATTRESSES] [-cns CHARACTERS,NUMBERS,SYMBOLS] -t TYPE"))
 		parser.add_argument("-H", dest="hash", required=True, help=": [-H] hash value")
-		parser.add_argument("-W", dest="word_list", required=True, help=": [-W] path word list")
-		parser.add_argument("-T", dest="types_of_hash", required=True, help=": [-T] Types of hash",
+		parser.add_argument("-w", dest="word_list", help=": [-w] path word list")
+		parser.add_argument("-m", dest="mattresses", help=": [-m] 	Number of mattresses")
+		parser.add_argument("-cns", dest="characters_numbers_symbols", help=": [-cns] characters/numbers/symbols values")
+		parser.add_argument("-t", dest="types_of_hash", required=True, help=": [-t] Types of hash",
 		choices=["md5","md4",
 		"sha1","sha224","sha256","sha512","sha384",
 		"sha3_224","sha3_256","sha3_384","sha3_512",
 		"blake2b","blake2s"])
 		args = parser.parse_args()
-		print("\nRaeding file...")
-		wordlist = open(args.word_list, "r")
-		num_lines = sum(1 for line in open(args.word_list))
-		lines = 0
 		number = args.hash.count("")
 		number -= 1
 		if (number == 32) and args.types_of_hash == "md5" :
@@ -72,40 +73,78 @@ def main():
 			exit(0)
 		datetime1 = datetime.datetime.now().replace(microsecond=0)
 		print("Start in {0}".format(datetime1))
-		for text in wordlist :
-			word = text
-			text = text.encode("utf-8")
-			hash = hashlib.new(args.types_of_hash)
-			hash.update(text)
-			text = hash.hexdigest()	
-			if text == args.hash :
-				datetime2 = datetime.datetime.now().replace(microsecond=0)
-				time_to_finish = datetime2-datetime1
-				print(
-					"\nStart in {0}\nFinish in {1}\nTime to finish {2}".format(
-						datetime1, datetime2, time_to_finish
+		if args.word_list != None :
+			print("\nRaeding file...")
+			wordlist = open(args.word_list, "r")
+			num_lines = sum(1 for line in open(args.word_list))
+			lines = 0
+			for text in wordlist :
+				word = text
+				text = text.encode("utf-8")
+				hash = hashlib.new(args.types_of_hash)
+				hash.update(text)
+				text = hash.hexdigest()	
+				if text == args.hash :
+					datetime2 = datetime.datetime.now().replace(microsecond=0)
+					time_to_finish = datetime2-datetime1
+					print(
+						"\nStart in {0}\nFinish in {1}\nTime to finish {2}".format(
+							datetime1, datetime2, time_to_finish
+						)
 					)
-				)
-				print("\n\n"+text,word)
-				exit()
-			elif text != args.hash or hashs:
-				lines += 1
-				op = (int((lines/num_lines)*100))
-				print(str(op)+"%",str(num_lines)+"/"+str(lines),end="\r")
-				pass
-			else :
-				print("[ERROR] using [-h] to help.!")
-				datetime2 = datetime.datetime.now().replace(microsecond=0)
-				time_to_finish = datetime2-datetime1
-				print(
-					"\nStart in {0}\nFinish in {1}\nTime to finish {2}".format(
-						datetime1, datetime2, time_to_finish
+					print("\n\n"+text,word)
+					exit()
+				elif text != args.hash or hashs:
+					lines += 1
+					op = (int((lines/num_lines)*100))
+					print(str(op)+"%",str(num_lines)+"/"+str(lines),end="\r")
+					pass
+				else :
+					print("[ERROR] using [-h] to help.!")
+					datetime2 = datetime.datetime.now().replace(microsecond=0)
+					time_to_finish = datetime2-datetime1
+					print(
+						"\nStart in {0}\nFinish in {1}\nTime to finish {2}".format(
+							datetime1, datetime2, time_to_finish
+						)
 					)
+		elif args.characters_numbers_symbols != None and args.mattresses != None :
+			for wl in itertools.product(args.characters_numbers_symbols, repeat=int(args.mattresses)):
+					try:
+						text = ''.join(wl)
+					except AttributeError:
+						text = string.join(wl, '')
+					word = text
+					text = text.encode("utf-8")
+					hash = hashlib.new(args.types_of_hash)
+					hash.update(text)
+					text = hash.hexdigest()	
+					if text == args.hash :
+						datetime2 = datetime.datetime.now().replace(microsecond=0)
+						time_to_finish = datetime2-datetime1
+						print(
+							"\nStart in {0}\nFinish in {1}\nTime to finish {2}".format(
+								datetime1, datetime2, time_to_finish
+							)
+						)
+						print("\n\n"+text,word)
+						exit()
+					else :
+						print(word,text,end="\r")
+						
+			datetime2 = datetime.datetime.now().replace(microsecond=0)
+			time_to_finish = datetime2-datetime1
+			print(
+				"\nStart in {0}\nFinish in {1}\nTime to finish {2}".format(
+					datetime1, datetime2, time_to_finish
 				)
+			)
+		else :
+			print("[ERROR] Using : python sniper-h.py -h")
 	except FileNotFoundError:
 		print("\n[ERROR] File not found.!")
 	except ValueError:
-		print("\n[ERROR] Hash type not found.!")
+		print("\n[ERROR] Enter just numbers -m [mattresses] !")
 	except KeyboardInterrupt:
 		pass
 if __name__ == "__main__" :
